0598fd407062f2b5d5a96f5862a1f74a9dd108cb
Vesicloud <vestatalebi@outlook.com>
2026-03-16 17:10:48 +0100

    firstcommit

---
+++ ConsoleApp1/ConsoleApp2/Program.cs
@@ -0,0 +1,19 @@
+﻿// 1
+Console.WriteLine("Enter a instrument:");
+string instrument = Console.ReadLine();
+Console.WriteLine($"Can you play {instrument}?");
+// 2 
+
+Console.WriteLine("Enter your family name:");
+string familyName = Console.ReadLine();
+Console.WriteLine("Enter your name:");
+string name = Console.ReadLine();
+
+Console.WriteLine($"{familyName} {name} has joined the firm");
+
+//3
+Console.WriteLine("Enter your fullname:");
+string fullname = Console.ReadLine();
+string[] fullnamesplit = fullname.Split(' ');
+
+Console.WriteLine($"{fullnamesplit[0].ToUpper()}  {fullnamesplit[1].ToLower()} : present");
